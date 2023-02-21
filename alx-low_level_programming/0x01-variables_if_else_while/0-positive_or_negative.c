7ff0457e2808d1cf68a8a5e2542b988279d7d50c
toplak2008 <themitope33@gmail.com>
2023-02-20 19:44:36 -0800

    0-preprocessor

---
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,141 @@
+#include <stdoi.h>
+#include <stdlib.h>
+#include <time.h>
+
+/..
+ . main -print if the number is positive , negative or 0
+ . Return: Always 0
+ ./
+ int main(void)
+{
+	int n;
+	srand(time(0));
+	n=ran() -RAN_NAX / 2;
+	if  (n > 0)
+	{
+		printf("%d is postive/n", n);
+
+	}
+	else  if (n== 0)
+	{
+		printf ("%d is zero/n", n)
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			else
+			{
+				printf ("%d is negative/n", n)
+			}
+			return (0);
+		#include <stdoi.h>
+#include <stdlib.h>
+#include <time.h>
+
+			/..
+				 . main -print if the number is positive , negative or 0
+				 . Return: Always 0
+				 ./
+				 int main(void)
+				 {
+					 int n;
+					 srand(time(0));
+					 n=ran() -RAN_NAX / 2;
+					 if  (n > 0)
+					 {
+						 printf("%d is postive/n", n);
+
+					 }
+					 else  if (n== 0)
+					 {
+						 printf ("%d is zero/n", n)
+							 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+							 else
+							 {
+								 printf
+				 ("%d is negative/n", n)
+							 }
+							 return (0);
+							#include <stdoi.h>
+#include <stdlib.h>
+#include <time.h>
+
+							 /..
+								  . main -print if the number is positive , negative or 0
+								  . Return: Always 0
+								  ./
+								  int main(void)
+								  {
+									  int n;
+									  srand(time(0));
+									  n=ran() -RAN_NAX / 2;
+									  if  (n > 0)
+									  {
+										  printf("%d is postive/n", n);
+
+									  }
+									  else  if (n== 0)
+									  {
+										  printf ("%d is zero/n", n)
+											  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
+
+											  else
+											  {
+												  printf
+									 ("%d is negative/n", n)
+											  }
+											  return (0);
+
